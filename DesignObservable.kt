4b4b2393e8dcf07238caade849ae8467258943f3
gl2206 <45152976+gl2206@users.noreply.github.com>
2021-08-10 20:07:01 +0200

    Create DesignObservable.kt

---
+++ DesignObservable.kt
@@ -0,0 +1,16 @@
+import kotlin.properties.Delegates
+
+fun main() {
+var observed = false
+var max: Int by Delegates.observable(0) { property, oldValue, newValue ->
+    println("" + property + " : " + oldValue + " -> " + newValue)
+    observed = true
+}
+
+println(max) // 0
+println("observed is ${observed}") // false
+
+max = 10
+println(max) // 10
+println("observed is ${observed}") // true
+}
